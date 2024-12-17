f2fcbd76fe91e8ac008c567f59325f3c6869356c
ETK <temirlan200370@email.com>
2024-12-17 17:14:35 +0500

    init

--- src/config/telegram.config.ts
+++ src/config/telegram.config.ts
@@ -1,3 +1,13 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+// Читаем файл token.txt из текущей директории
+const tokenFilePath = path.join(__dirname, 'token.txt');
+const token = fs.existsSync(tokenFilePath)
+    ? fs.readFileSync(tokenFilePath, 'utf-8').trim()
+    : '';
+
+// Конфигурация Telegram
 export const telegramConfig = {
-    botToken: process.env.TELEGRAM_BOT_TOKEN || '7920925808:AAE4u4vzIj-3EZdV6vIyQXxVa5N9PgkPYOY',
+    botToken: process.env.TELEGRAM_BOT_TOKEN || token,
 };
